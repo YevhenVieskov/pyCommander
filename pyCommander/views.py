e6694476a035540728d4854a70ab5f325feab7ac
yevhenv <yevhen.vieskov@dat.com>
2023-01-12 23:22:53 +0200

    files menu actions created

--- pyCommander/views.py
+++ pyCommander/views.py
@@ -19,7 +19,6 @@ from PyQt5.QtWidgets import (
 initWidth = 800
 initHeigh = 600
 
-
 '''def _createMenuBar(self):
     menuBar = self.menuBar()
     # File menu
@@ -64,10 +63,61 @@ initHeigh = 600
         self.helpContentAction = QAction("&Help Content", self)
         self.aboutAction = QAction("&About", self)
 '''
+class packAction(QAction):
+    pass
+
+class  unpackSpecificFileAction(QAction):
+    pass
+
+class  testArchivesAction(QAction):
+    pass
+
+class  compareByContentAction(QAction):
+    pass
+
+class  associateWithAction(QAction):
+    pass
+
+class  internalAssociationAction(QAction):
+    pass
+
+class  propertiesAction(QAction):
+    pass
+
+class  calculateOccupiedSpaceAction(QAction):
+    pass
+
+class  multiRenameToolAction(QAction):
+    pass
+
+class  editCommentAction(QAction):
+    pass
+
+class  printAction(QAction):
+    pass
+
+class  splitFileAction(QAction):
+    pass
+
+class  combineFileAction(QAction):
+    pass
+
+class  encodeFileAction(QAction):
+    pass
+
+class  decodeFileAction(QAction):
+    pass
+
+class  createCheckSumAction(QAction):
+    pass
+
+class  verifyCheckSumAction(QAction):
+    pass
+
 
 class exitAction(QAction):
 
-    def __init__(self,parent):
+    def __init__(self, parent):
         super(exitAction, self).__init__(QIcon('exit.png'), '&Exit', parent)
         self.setShortcut('Ctrl+Q')
         self.setStatusTip('Exit application')
@@ -75,131 +125,46 @@ class exitAction(QAction):
 
 
 
-#menu5.py
 
-class TitleBar(QWidget):
-    height = 35
+# menu5.py
+
+class MenuBar(QWidget):
+
     def __init__(self, parent):
-        super(TitleBar, self).__init__()
+        super(MenuBar, self).__init__()
         self.parent = parent
         self.layout = QHBoxLayout()
-        self.layout.setContentsMargins(0,0,0,0)
-
-
-
+        self.layout.setContentsMargins(0, 0, 0, 0)
         self.menu_bar = QMenuBar()
-        self.menu_bar.setStyleSheet("""
-            color: #fff;
-            background-color: #23272A;
-            font-size: 14px;
-            padding: 4px; 
-        """)
-        self.menu_file = self.menu_bar.addMenu('file')
-        self.menu_file_open=self.menu_file.addAction('open')
-        self.menu_file_save=self.menu_file.addAction('save')
-        self.menu_file_saveas=self.menu_file.addAction('save as...')
-        self.menu_file_quit=self.menu_file.addAction('exit')
-        self.menu_file_quit.triggered.connect(qApp.quit)
+        self.menu_file = self.menu_bar.addMenu("&Files")
+        self.menu_file_open = self.menu_file.addAction(packAction)
+        self.menu_file_open = self.menu_file.addAction(unpackSpecificFileAction)
+        self.menu_file_open = self.menu_file.addAction(testArchivesAction)
+        self.menu_file_open = self.menu_file.addAction(compareByContentAction)
+        self.menu_file_open = self.menu_file.addAction(associateWithAction)
+        self.menu_file_open = self.menu_file.addAction(internalAssociationAction)
+        self.menu_file_open = self.menu_file.addAction(propertiesAction)
+        self.menu_file_open = self.menu_file.addAction(calculateOccupiedSpaceAction)
+        self.menu_file_open = self.menu_file.addAction(multiRenameToolAction)
+        self.menu_file_open = self.menu_file.addAction(editCommentAction)
+        self.menu_file_open = self.menu_file.addAction(printAction)
+        self.menu_file_open = self.menu_file.addAction(splitFileAction)
+        self.menu_file_open = self.menu_file.addAction(combineFileAction)
+        self.menu_file_open = self.menu_file.addAction(encodeFileAction)
+        self.menu_file_open = self.menu_file.addAction(decodeFileAction)
+        self.menu_file_open = self.menu_file.addAction(createCheckSumAction)
+        self.menu_file_open = self.menu_file.addAction(verifyCheckSumAction)
+        self.menu_file_open = self.menu_file.addAction(exitAction)
 
-        self.menu_work=self.menu_bar.addMenu('work')
-        self.menu_analysis=self.menu_bar.addMenu('analysis')
-        self.menu_edit=self.menu_bar.addMenu('edit')
-        self.menu_window=self.menu_bar.addMenu('window')
-        self.menu_help=self.menu_bar.addMenu('help')
+        self.menu_mark = self.menu_bar.addMenu("&Mark")
+        self.menu_commands = self.menu_bar.addMenu("&Commands")
+        self.menu_net = self.menu_bar.addMenu("&Net")
+        self.menu_show = self.menu_bar.addMenu("&Show")
+        self.menu_configuration = self.menu_bar.addMenu("&Configuration")
+        self.menu_start = self.menu_bar.addMenu("&Start")
 
         self.layout.addWidget(self.menu_bar)
-
-        self.title = QLabel("Hello World!")
-        self.title.setFixedHeight(self.height)
-        self.layout.addWidget(self.title)
-        self.title.setStyleSheet("""
-            background-color: #23272a;  /* 23272a   #f00*/
-            font-weight: bold;
-            font-size: 16px;
-            color: blue;
-            padding-left: 170px;
-        """)
-
-        self.closeButton = QPushButton(' ')
-        self.closeButton.clicked.connect(self.on_click_close)
-        self.closeButton.setStyleSheet("""
-            background-color: #DC143C;
-            border-radius: 10px;
-            height: {};
-            width: {};
-            margin-right: 3px;
-            font-weight: bold;
-            color: #000;
-            font-family: "Webdings";
-            qproperty-text: "r";
-        """.format(self.height/1.7,self.height/1.7))
-
-        self.maxButton = QPushButton(' ')
-        self.maxButton.clicked.connect(self.on_click_maximize)
-        self.maxButton.setStyleSheet("""
-            background-color: #32CD32;
-            border-radius: 10px;
-            height: {};
-            width: {};
-            margin-right: 3px;
-            font-weight: bold;
-            color: #000;
-            font-family: "Webdings";
-            qproperty-text: "1";
-        """.format(self.height/1.7,self.height/1.7))
-
-        self.hideButton = QPushButton(' ')
-        self.hideButton.clicked.connect(self.on_click_hide)
-        self.hideButton.setStyleSheet("""
-            background-color: #FFFF00;
-            border-radius: 10px;
-            height: {};
-            width: {};
-            margin-right: 3px;
-            font-weight: bold;
-            color: #000;
-            font-family: "Webdings";
-            qproperty-text: "0";
-        """.format(self.height/1.7,self.height/1.7))
-
-        self.layout.addWidget(self.hideButton)
-        self.layout.addWidget(self.maxButton)
-        self.layout.addWidget(self.closeButton)
         self.setLayout(self.layout)
-
-        self.start = QPoint(0, 0)
-        self.pressing = False
-        self.maximaze = False
-
-    def resizeEvent(self, QResizeEvent):
-        super(TitleBar, self).resizeEvent(QResizeEvent)
-        self.title.setFixedWidth(self.parent.width())
-
-    def mousePressEvent(self, event):
-        self.start = self.mapToGlobal(event.pos())
-        self.pressing = True
-
-    def mouseMoveEvent(self, event):
-        if self.pressing:
-            self.end = self.mapToGlobal(event.pos())
-            self.movement = self.end-self.start
-            self.parent.move(self.mapToGlobal(self.movement))
-            self.start = self.end
-
-    def mouseReleaseEvent(self, QMouseEvent):
-        self.pressing = False
-
-    def on_click_close(self):
-        sys.exit()
-
-    def on_click_maximize(self):
-        self.maximaze = not self.maximaze
-        if self.maximaze:    self.parent.setWindowState(Qt.WindowNoState)
-        if not self.maximaze:
-            self.parent.setWindowState(Qt.WindowMaximized)
-
-    def on_click_hide(self):
-        self.parent.showMinimized()
 
 
 '''class StatusBar(QWidget):
@@ -230,6 +195,7 @@ class TitleBar(QWidget):
 
 class Window(QMainWindow):
     """Main Window."""
+
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
@@ -237,8 +203,8 @@ class Window(QMainWindow):
         self.resize(800, 600)
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
-        self.title_bar = TitleBar(self)
-        #self.status_bar = StatusBar(self)
+        self.title_bar = MenuBar(self)
+        # self.status_bar = StatusBar(self)
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.title_bar)
         self.centralWidget.setLayout(self.layout)
@@ -250,7 +216,6 @@ class Window(QMainWindow):
         self.layout.addWidget(self.status_bar)
         self.layout.setSpacing(0)
         self.setLayout(self.layout)"""
-
 
     """def initUI(self):
         hbox = QHBoxLayout(self)
